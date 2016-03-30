e07e1efc5f162e5dd50e655836b5542a18943344
Jedidiah Avelino <jed.avelino@gmail.com>
2016-03-30 17:41:30 +0800

    implement post loop and custom body class

--- wp-content/themes/wordpress101theme/header.php
+++ wp-content/themes/wordpress101theme/header.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<title>Wordpress 101</title>
 	<?php wp_head(); ?>
 </head>
-<body>
+
+<?php
+
+	if( is_front_page() ) :
+		$wordpress101_classes = array( 'wordpress101-class', 'my-class' );
+	else :
+		$wordpress101_classes = array( 'no-wordpress101-class' );
+	endif;
+
+?>
+
+<body <?php body_class( $wordpress101_classes ); ?>>
 	
 	<!-- Main Header Navigation -->
 	<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
--- wp-content/themes/wordpress101theme/index.php
+++ wp-content/themes/wordpress101theme/index.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
 
-<h1>This is my index.</h1>
+	<?php if( have_posts(  ) ) : ?>
+
+		<?php while( have_posts() ) : the_post(); ?>
+
+			<!-- Display Content of Post -->
+			<h3><?php the_title(); ?></h3>
+			<small>Posted on <?php the_time( 'F j, Y' ) ?> at <?php the_time( 'g:i a' ); ?> in <?php the_category(); ?></small>
+
+			<p><?php the_content(); ?></p>
+
+			<hr>
+
+		<?php endwhile; ?>
+
+	<?php endif; ?>
 
 <?php get_footer(); ?>
