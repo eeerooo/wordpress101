9e23dd0c352ce5cc22c1f5a2549a72521c2b0805
Jedidiah Avelino <jed.avelino@gmail.com>
2016-04-01 02:20:32 +0800

    edited query_posts with wp_query

---
+++ wp-content/themes/wordpress101theme/page-home.php
@@ -0,0 +1,93 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+	<div class="col-xs-12">
+		
+		<?php
+
+			$lastBlog = new WP_Query( 'type=post&posts_per_page=1' );
+
+			if( $lastBlog -> have_posts(  ) ) :
+
+				while( $lastBlog -> have_posts() ) : $lastBlog -> the_post(); ?>
+
+					<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<?php endwhile;
+
+			endif;
+
+			wp_reset_postdata();
+
+		?>
+
+	</div>
+
+	<div class="col-xs-12 col-sm-8">
+
+		<?php if( have_posts(  ) ) : ?>
+
+			<?php while( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', get_post_format() ); ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+		<?php
+
+			// print other 2 posts not the first one
+			$args = array(
+				'type'				=>	'post',
+				'posts_per_page'	=>	2,
+				'offset'			=>	1
+
+			);
+
+			$lastBlog = new WP_Query( $args );
+
+			if( $lastBlog -> have_posts(  ) ) :
+
+				while( $lastBlog -> have_posts() ) : $lastBlog -> the_post(); ?>
+
+					<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<?php endwhile;
+
+			endif;
+
+			wp_reset_postdata();
+
+		?>
+
+		<hr>
+
+		<?php
+
+			// print only asia
+			$lastBlog = new WP_Query( 'type=post&posts_per_page=-1&category_name=asia' );
+
+			if( $lastBlog -> have_posts(  ) ) :
+
+				while( $lastBlog -> have_posts() ) : $lastBlog -> the_post(); ?>
+
+					<?php get_template_part( 'content', get_post_format() ); ?>
+
+				<?php endwhile;
+
+			endif;
+
+			wp_reset_postdata();
+
+		?>
+
+	</div>
+
+	<div class="col-xs-12 col-sm-4">
+		<?php get_sidebar(); ?>
+	</div>
+
+</div>
+<?php get_footer(); ?>
