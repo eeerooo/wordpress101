d6df5736199dc307211268af0d8d5f55dd73eaa6
Jedidiah Avelino <jed.avelino@gmail.com>
2016-04-13 05:50:19 +0800

    wordpress live development session part 2, session 1

--- wp-content/themes/wordpress101theme/page-home.php
+++ wp-content/themes/wordpress101theme/page-home.php
@@ -1,42 +1,84 @@
 <?php get_header(); ?>
 
 <div class="row">
-		
-	<?php
 
-		$args_cat = array(
-			'include'		=> '7, 13',
-		);
+	<div id="awesome-carousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators --
+		<ol class="carousel-indicators">
+			<li data-target="#awesome-carousel" data-slide-to="0" class="active"></li>
+			<li data-target="#awesome-carousel" data-slide-to="1"></li>
+			<li data-target="#awesome-carousel" data-slide-to="2"></li>
+		</ol>
 
-		$categories = get_categories( $args_cat );
-		foreach( $categories as $category ):
+		<!-- wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
 
-			$args = array(
-				'type'				=>	'post',
-				'posts_per_page'	=>	1,
-				'category__in'		=> $category -> term_id,
-				'category__not_in'	=> array( 1 )
-			);
+			<?php
 
-			$lastBlog = new WP_Query( $args );
+				$args_cat = array(
+					'include'		=> '7, 13',
+				);
 
-			if( $lastBlog -> have_posts() ) :
+				$categories = get_categories( $args_cat );
+				$count = 0;
+				$bullets = '';
+				foreach( $categories as $category ):
 
-				while( $lastBlog -> have_posts() ) : $lastBlog -> the_post(); ?>
-					
-					<div class="col-xs-12 col-sm-4">
-						<?php get_template_part( 'content', 'featured' ); ?>
-					</div>
+					$args = array(
+						'type'				=>	'post',
+						'posts_per_page'	=>	1,
+						'category__in'		=> $category -> term_id,
+						'category__not_in'	=> array( 1 )
+					);
 
-				<?php endwhile;
+					$lastBlog = new WP_Query( $args );
 
-			endif;
+					if( $lastBlog -> have_posts() ) :
 
-			wp_reset_postdata();
+						while( $lastBlog -> have_posts() ) : $lastBlog -> the_post(); ?>
+							
+							<div class="item <?php if( $count == 0 ) : echo 'active'; endif; ?>">
+								<?php the_post_thumbnail( 'full' ); ?>
+								<div class="carousel-caption">
+									<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
+									<small><?php the_category(); ?></small>
+								</div>
+							</div>
 
-		endforeach;
+							<?php $bullets .= '<li data-target="#awesome-carousel" data-slide-to="'.$count.'" class="'; ?>
 
-	?>
+								<?php if( $count == 0 ) : $bullets .= 'active'; endif; ?> 
+
+								<?php $bullets .= '"></li>'; ?>
+
+						<?php endwhile;
+
+					endif;
+
+					wp_reset_postdata();
+
+					$count++;
+
+				endforeach;
+			?>
+			
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<?php echo $bullets; ?>
+			</ol>
+
+		</div>
+
+		<!-- controls -->
+		<a class="left carousel-control" href="#awesome-carousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#awesome-carousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
 
 </div>
 
